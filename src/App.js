da51180b67f0677e4e43166aec5b5a50172db6f2
augusto <augusdpmenezes@gmail.com>
2023-03-03 18:06:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> novo commit
+          Edit <code>src/App.js</code> novo commit aqui
         </p>
         <a
           className="App-link"
