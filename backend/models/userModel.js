a81dce1a9b137933db64b2a6c41989bfb3f4a708
vnniciusg109 <90412694+vnniciusg109@users.noreply.github.com>
2023-03-10 20:18:58 -0300

    confirmação de senha

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,12 +8,22 @@ const UserSchema = new mongose.Schema ({
     email:{type:String,required:true},
     useremail:{type:String,required:true},
     userpassword1:{type:String, required:true},
+    userpassword2:{type:String, required:true},
     role:{type:String,enum:['user','barber','admin'],default:'user'}
 
 },{
     timestamps:true
-}
-)
+})
+
+UserSchema.pre('save', function(next) {
+    if (this.userpassword1 !== this.userpassword2) {
+      const err = new Error('As senhas não são iguais');
+      err.status = 400;
+      return next(err);
+    }
+    next();
+  });
+  
 
 
 const User = mongose.model('User',UserSchema);
